6adf7faf3d12d54b2a4cacdc8b0d06ba398cb78c
Walter Santizo <walterjav19@gmail.com>
2024-06-12 20:07:57 -0600

    oddEvenv2

---
+++ oddv2.s
@@ -0,0 +1,53 @@
+.global _start
+
+_start:
+    // CARGAMOS EL MENSAJE EN PANTALLA
+    mov x0, 1       // set stdout
+    ldr x1, =msg    // load msg
+    mov x2, 14      // size msg
+    mov x8, 64      // write syscall_num
+    svc 0           // generic syscall
+
+    // CARGAMOS EL TEXTO
+    mov x0, 0       // set stdin
+    ldr x1, =input    // load buffer
+    mov x2, 2      // size buffer
+    mov x8, 63      // read syscall_num
+    svc 0           // generic syscall
+
+    ldr x0, =input  // load number
+    ldrb w1, [x0]   // load 1st digit
+    sub w1, w1, 48  // ascii to num
+    and w2, w1, 1   // check parity
+
+    cmp w2, 0
+    bne ODD_NUMBER
+
+    EVEN_NUMBER:
+        mov x0, 1       // set stdout
+        ldr x1,=msg1     // load msg
+        mov x2, 7      // size msg
+        mov x8, 64      // write syscall_num
+        svc 0
+        b CLOSE
+
+    ODD_NUMBER:
+        mov x0, 1       // set stdout
+        ldr x1,=msg2     // load msg
+        mov x2, 8      // size msg
+        mov x8, 64      // write syscall_num
+        svc 0
+    CLOSE:
+        mov x0, 0       // set stdout
+        ldr x8, 93    // load msg
+        svc 0
+    
+
+
+.data
+msg:
+   msg:   .ascii "Enter a text: "
+   msg1:  .ascii "Is odd\n"
+   msg2: .ascii "Is even\n"
+buf:
+  input:  .space 2
