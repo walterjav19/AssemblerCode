0e9466082d6aa84009d801d916b3bf9b2507bc10
Walter Santizo <walterjav19@gmail.com>
2024-06-18 23:04:10 -0600

    hola mundo

--- test.s
+++ test.s
@@ -1,5 +1,20 @@
+.section .data
+message:
+    .ascii "Hola Mundo!\n"
+len = . - message
+
+.section .text
 .global _start
+
 _start:
-  mov x0, 5
-  mov x8, 93
-  svc 0
+    // Escribir el mensaje
+    mov x0, 1          // File descriptor 1: stdout
+    ldr x1, =message   // Dirección del mensaje
+    ldr x2, =len       // Longitud del mensaje
+    mov x8, 64         // Número de llamada del sistema para sys_write (en AArch64)
+    svc 0              // Llamada al sistema
+
+    // Salir del programa
+    mov x0, 0          // Estado de salida
+    mov x8, 93         // Número de llamada del sistema para sys_exit (en AArch64)
+    svc 0              // Llamada al sistema
